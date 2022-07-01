a4ae5c8e398a7ea7e6e280fe7d3bdc621269cda9
Renan <renan.aragorn@gmail.com>
2022-07-01 01:07:27 -0300

    server structure done

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const express = require("express")
+
+const app = express()
+
+app.get("/", (request, response) => {
+  response.send("Hello World!")
+})
+
+const PORT = 3333
+app.listen(PORT, () => console.log(`Server is Running on Port ${PORT}`))
