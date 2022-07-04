5932cb3e83e25a0ab55cc3608ea81cdcdb1758f3
Renan <renan.aragorn@gmail.com>
2022-07-04 20:32:19 -0300

    user controller and error treatment class created

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+require("express-async-errors")
+
+const AppError = require("./utils/AppError")
 const express = require("express")
 
 const routes = require("./routes")
@@ -11,5 +14,20 @@ app.get("/", (request, response) => {
   response.send("Hello World!")
 })
 
+app.use(( error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "Error",
+      message: error.message
+    })
+  }
+
+  console.error(error)
+
+  return response.status(500).json({
+    status: "Error",
+    message: "Internal server error"
+  })
+})
 const PORT = 3333
 app.listen(PORT, () => console.log(`Server is Running on Port ${PORT}`))
