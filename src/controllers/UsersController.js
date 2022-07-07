9311d4bb8e64c8c016a5f3fb9931e41bca686d3c
Renan <renan.aragorn@gmail.com>
2022-07-07 00:10:38 -0300

    tags controller and index method done

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -4,6 +4,9 @@ const AppError = require("../utils/AppError")
 
 const sqliteConnection = require("../database/sqlite")
 
+const knex = require("../database/knex")
+
+
 class UsersController {
   async create(request, response) {
     const { name, email, password } = request.body
